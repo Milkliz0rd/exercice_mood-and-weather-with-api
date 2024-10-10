10155453f76b81f7fd0493f8b2ddafe4ccd15083
Morozoff Alexandre <morozoff_alexandre@outlook.com>
2024-10-10 16:48:19 +0200

    tous les fichiers js sont commentés

--- report-data.js
+++ report-data.js
@@ -1,12 +1,22 @@
+//--------- Récupération des données dans le local storage----------------
+
+// on créé une fonction qui sera exporté pour récupéré nos donnés du localStorage
 export function getReports() {
+  // on créé une variable "reports" qui utilise "JSON.parse" pour convertir les données JSON (langage du localStorage) en js
+  // ette fonction récupère (getItem) les données d'humeurs stockées dans le localStorage sous la clé 'moodReports'. Si aucune donnée ("||") n'existe, elle retourne un tableau vide ('[]')
     const reports = JSON.parse(localStorage.getItem('moodReports') || '[]')
-  
+    // on return la variable
     return reports
   }
   
+  //------- Sauvegarde des données dans le localStorage--------------
+
+// on créé une fonction qui sera exporté pour ajouter un nouveau rapport d'humeur à la liste des rapports existants dans le localStorage 
   export function saveReport(report) {
+    // on créé la variable "reports" qui fait appel à la fonction "getReports"
     const reports = getReports()
-  
+    // "reports.push(report)" ajoute le nouveau rapport à la fin du tableau
     reports.push(report)
+    // on sauvegarde celle-ci dans le localstorage
     localStorage.setItem('moodReports', JSON.stringify(reports))
   }
--- weather.js
+++ weather.js
@@ -2,9 +2,10 @@
 
 import { getReports, saveReport } from "./report-data.js";
 
+//----------Fonction d'ajout d'humeur et de météo d'Orléans--------------------
+
 // Création de la fonction pour ajouter une nouvelle humeur à notre serveur
 function addNewMoodEventListener() {
-  console.log("it works")
   // on va chercher l'endroit où notre humeur est rentré (ici c'est un formulaire)
   const moodForm = document.querySelector('.mood-form');
   // on ajoute le listener sur l'évènement de notre submit
@@ -13,39 +14,54 @@ function addNewMoodEventListener() {
     event.preventDefault();
   // on créé une variable mood qui sera la valeur de l'input de moodForm
     const mood = event.target.querySelector('[name=mood]').value;
-
-    //------------------ Send the mood to your server here --------------------------
-
-    // On va récupérer notre serveur avec fetch et .then pour la réponse du serveur
+  // On va récupérer notre serveur avec fetch et .then pour la réponse du serveur
     fetch("https://api.openweathermap.org/data/2.5/weather?lat=47.90289&lon=1,90389&appid=964c92af9abfd53d27f21227cb8eab94")
       .then(response => response.json())
-      // On récupère les "data" avec .then du temps qu'il fait en ce moment à Orléans et on en fait une variable
+      // On récupère les "data" avec .then et on en fait une variable qui contient les données météo d'Orléans
       .then(data => {
         const weather = data
-
+        // on envoie les valeurs de "mood" et de "weather" à une fonction "saveMoodAndRelatedWeather" 
         saveMoodAndRelatedWeather(mood, weather)
+        // on affiche le résultat via une fonction "displayReports"
         displayReports()
       })
 
   });
 }
-
+// on appel la fonction "addNewMoodEventListener"
 addNewMoodEventListener()
 
+
+//------------ Fonction d'envoie à la sauvegarde du localStorage---------------
+
+// création de la fonction "saveMoodAndRelatedWeather" en y ajoutant les variables "mood" et "weather"
 function saveMoodAndRelatedWeather(mood, weather) {
+  // création de la variable report qui contient les objets
   const report = {
+    //mood
     mood,
+    //weather qui indique le "weather" et "description" extraite de la data
     weather: weather.weather[0].description,
+    // timestamp avec la date
     timestamp: new Date()
   }
-
+  // report est ensuite sauvegarder grace à la fonction "saveReport" qui se trouve dans le fichier "./report-data.js"
   saveReport(report)
 }
 
+
+
+//----- fonction d'affichage à l'écran de notre humeur et météo -------------
+
+// On créé la fonction "displayReports" pour afficher notre humeur et le temps qu'il fait 
 function displayReports() {
+  // On créé une variable qui fera appel à la fonction "getReports" que l'on trouve sur le fichier "./report-data.js"
   const reports = getReports()
+  // On va chercher dans le DOM où notre fonction va s'afficher
   const moodList = document.querySelector("#mood-list")
+  // on crée une variable "reportsList" qui va créer une liste avec la fonction "map" en prenant compte des donnés de "reports"
   const reportsList = reports.map(report => {
+    //cette liste est "return" puis incrémentée en html
     return `
         <li>
           <p>mood: ${report.mood}</p>
@@ -54,6 +70,6 @@ function displayReports() {
         </li>
     `
   })
-
+  // "moodlist.innerhtml" est suite mis à jour avec la liste généré "reportList"
   moodList.innerHTML = reportsList
 }
